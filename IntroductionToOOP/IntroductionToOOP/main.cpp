443b5ecca0414794490e97ff2cac9c29ef68fe82
Clayman <Clayman@DESKTOP-DM8E77U>
2024-06-05 10:15:14 +0300

    Constructors in progress

--- IntroductionToOOP/IntroductionToOOP/main.cpp
+++ IntroductionToOOP/IntroductionToOOP/main.cpp
@@ -1,12 +1,61 @@
 ﻿//Объектно-ориентированное программирование на языке C++
 #include<iostream>
 using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
 
 //Объявляя структуру или класс мы создаем новый тип данных (пользовательский тип данных).
 class Point
 {
 	double x;
 	double y;
+public:
+	double get_x()const
+	{
+		return x;
+	}
+	double get_y()const
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this->x = x;
+	}
+	void set_y(double y)
+	{
+		this->y = y;
+	}
+
+	//				Constrcutors:
+	Point()
+	{
+		x = y = 0;
+		cout << "DefaultConstructor:\t" << this << endl;
+	}
+	Point(double x)
+	{
+		this->x = x;
+		this->y = 0;
+		cout << "1ArgConstructor:\t" << this << endl;
+	}
+	Point(double x, double y)
+	{
+		this->x = x;
+		this->y = y;
+		cout << "Constrcutor:\t\t" << this << endl;
+	}
+	~Point()
+	{
+		cout << "Destructor:\t\t" << this << endl;
+	}
+
+	//				Methods:
+	void print()const
+	{
+		cout << "X = " << x << "\tY = " << y << endl;
+	}
 };
 
 //#define STRUCT_POINT
@@ -28,8 +77,25 @@ void main()
 	cout << pA->x << "\t" << pA->y << endl;
 #endif // STRUCT_POINT
 
-	Point A;
-	cout << A.x << "\t" << A.y << endl;
+	Point A;		//Default constructor
+	//A.set_x(2);
+	//A.set_y(3);
+	cout << A.get_x() << "\t" << A.get_y() << endl;
+	A.print();
+
+	Point B = 5;	//Single-Argument Constructor
+	B.print();
+
+	Point C(2, 3);
+	C.print();
+
+//	for (counter;  condition; increment)
+	/*for (int i = 0; i < 10; i++)
+	{
+		cout << i << "\t";
+	}*/
+	//cout << i << endl;
+	//cout << endl;
 }
 
 /*
@@ -42,6 +108,28 @@ void main()
 /*
 -------------------------------------------------------------
 				OOP concepts:
+1. Encapsulation - это сокрытие определенной части класса от внешнего мира;
+	Модификаторы доступа:
+		private:
+		public:
+		protected:
+	get/set-методов
+	get-методы (взять, получить)
+	set-методы (задать, установить)
+2. Inheritance;
+3. Polymorphism;
+-------------------------------------------------------------
+*/
 
+/*
+-------------------------------------------------------------
+1. Constructor - это метод, который создает объект;
+	- с параметрами;
+	- без параметров;
+	- по умолчанию - это конструктор, который может быть вызван без параметров;
+	- конструктор копирования;
+	- конструктор переноса;
+2. ~Destructor - это метод, который уничтожает обеъкт по истечении его времени жизни;
+3. Assignment operator;
 -------------------------------------------------------------
 */
