952ad332e0c89cd9098574e01f3d33fa640ab13e
User <User@DESKTOP-0TCDSR6>
2024-07-06 21:27:16 +0300

    Files day 1

--- Inheritance/Academy/main.cpp
+++ Inheritance/Academy/main.cpp
@@ -4,6 +4,8 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+#define delimiter "\n---------------------------------------------\n"
+
 #define HUMAN_TAKE_PARAMETERS const std::string& last_name, const std::string& first_name, int age
 #define HUMAN_GIVE_PARAMETERS last_name, first_name, age
 
@@ -46,17 +48,22 @@ public:
 		set_age(age);
 		cout << "HConstructor:\t" << this << endl;
 	}
-	~Human()
+	virtual ~Human()
 	{
 		cout << "HDestructor:\t" << this << endl;
 	}
 
 	//				Methods:
-	void print()const
+	virtual std::ostream& print(std::ostream& os)const
 	{
-		cout << last_name << " " << first_name << " " << age << endl;
+		return os << last_name << " " << first_name << " " << age;
 	}
 };
+
+std::ostream& operator<<(std::ostream& os, const Human& obj)
+{
+	return obj.print(os);
+}
 
 #define STUDENT_TAKE_PARAMETERS const std::string& speciality, const std::string& group, double rating, double attendance
 #define STUDENT_GIVE_PARAMETERS speciality, group, rating, attendance
@@ -102,7 +109,7 @@ public:
 	}
 
 	//				Constructors:
-	Student(HUMAN_TAKE_PARAMETERS,STUDENT_TAKE_PARAMETERS):Human(HUMAN_GIVE_PARAMETERS)
+	Student(HUMAN_TAKE_PARAMETERS, STUDENT_TAKE_PARAMETERS) :Human(HUMAN_GIVE_PARAMETERS)
 	{
 		set_speciality(speciality);
 		set_group(group);
@@ -116,10 +123,9 @@ public:
 	}
 
 	//				Methods:
-	void print()const
+	std::ostream& print(std::ostream& os)const override
 	{
-		Human::print();
-		cout << speciality << " " << group << " " << rating << " " << attendance << endl;
+		return Human::print(os)<< " " << speciality << " " << group << " " << rating << " " << attendance;
 	}
 };
 
@@ -160,23 +166,100 @@ public:
 	}
 
 	//					Methods:
-	void print()const
+	std::ostream& print(std::ostream& os)const override
 	{
-		Human::print();
-		cout << speciality << " " << experience << " years" << endl;
+		return Human::print(os) << " " << speciality << " " << experience << " years";
 	}
 };
+#define GRADUATE_TAKE_PARAMETERS const std::string& subject
+#define GRADUATE_GIVE_PARAMETERS subject
+class Graduate :public Student
+{
+	std::string subject;
+public:
+	const std::string& get_topic()const
+	{
+		return subject;
+	}
+	void set_topic(const std::string& topic)
+	{
+		this->subject = topic;
+	}
+
+	//			Constructors:
+	Graduate(HUMAN_TAKE_PARAMETERS, STUDENT_TAKE_PARAMETERS, GRADUATE_TAKE_PARAMETERS) : Student(HUMAN_GIVE_PARAMETERS, STUDENT_GIVE_PARAMETERS) {
+		set_topic(subject);
+		cout << "GConstructor:\t" << this << endl;
+	}
+	Graduate(const Student& student, const std::string& topic) :Student(student)
+	{
+		set_topic(topic);
+		cout << "GConstructor:\t" << this << endl;
+	}
+	~Graduate()
+	{
+		cout << "GDestructor:\t" << this << endl;
+	}
+
+	//					Methods:
+	std::ostream& print(std::ostream& os)const override
+	{
+		return Student::print(os) << " " << subject;
+	}
+};
+
+void Print(Human* group[], const int n)
+{
+	cout << delimiter << endl;
+	for (int i = 0; i < n; i++)
+	{
+		//group[i]->print();
+		cout << *group[i] << endl;
+		cout << delimiter << endl;
+	}
+}
+void Clear(Human* group[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		delete group[i];
+	}
+}
+
+//#define INHERITANCE
 
 void main()
 {
 	setlocale(LC_ALL, "");
 	cout << "HelloAcademy" << endl;
+
+#ifdef INHERITANCE
+	cout << delimiter << endl;
 	Human human("Richter", "Jeffrey", 40);
 	human.print();
+	cout << delimiter << endl;
 
 	Student student("Pinkman", "Jessie", 20, "Chenistry", "WW_220", 95, 90);
 	student.print();
+	cout << delimiter << endl;
 
 	Teacher teacher("White", "Walter", 50, "Chemistry", 25);
 	teacher.print();
+	cout << delimiter << endl;
+
+	Graduate graduate("Schrader", "Hank", 40, "Criminalistic", "OBN", 70, 75, "How to catch Heisenberg");
+	graduate.print();
+	cout << delimiter << endl;
+#endif // INHERITANCE
+
+	Human* group[] =
+	{
+		new Student("Pinkman", "Jessie", 20, "Chenistry", "WW_220", 95, 90),
+		new Teacher("White", "Walter", 50, "Chemistry", 25),
+		new Graduate("Schrader", "Hank", 40, "Criminalistic", "OBN", 70, 75, "How to catch Heisenberg"),
+		new Student("Vercetti", "Tommy", 30, "Theft", "Vice", 97, 98),
+		new Teacher("Diaz", "Ricardo", 50, "Weapons distribution", 20)
+	};
+	Print(group, sizeof(group) / sizeof(group[0]));
+	Clear(group, sizeof(group) / sizeof(group[0]));
 }
